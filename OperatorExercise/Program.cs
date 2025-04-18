8bc678964c74f945ee3412ff24b425141f0bf001
Pennylane79 <wrichard3417@hotmail.com>
2025-04-18 13:44:56 -0500

    Completed Operators-Exercise

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+
+            int sum = a + b;
+            int diff = a - b;
+            int prod = a * b;
+
+            int qout = a / b;
+            int rem = a % b;
+
+            Console.WriteLine($"{a}/{b} ={qout}have{sum}a{diff}wonderful{prod}time{rem}");
+
+               
+
+
         }
     }
 }
